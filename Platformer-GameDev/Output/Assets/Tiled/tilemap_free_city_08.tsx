ad1aaade1df252aeaa5e7a3641420f4323921664
gosu00 <125640161+gosu00@users.noreply.github.com>
2023-10-17 18:16:49 +0200

    map implementation

--- Platformer-GameDev/Output/Assets/Tiled/tilemap_free_city_08.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="tilemap_free_city_08" tilewidth="30" tileheight="30" tilecount="432" columns="27">
- <image source="../../../../../../../Downloads/tilemap_free_city_08.png" width="813" height="488"/>
-</tileset>
