101fa0783aadd30d89a4acee86fb695a03890674
gosu00 <125640161+gosu00@users.noreply.github.com>
2023-12-20 16:25:07 +0100

    rey clean up

--- Platformer-GameDev/Game/Source/Rey.cpp
+++ Platformer-GameDev/Game/Source/Rey.cpp
@@ -23,7 +23,6 @@ bool Rey::Awake() {
 	position.x = parameters.attribute("x").as_int();
 	position.y = parameters.attribute("y").as_int();
 	texturePath = parameters.attribute("texturepath").as_string();
-	//speed = parameters.attribute("speed").as_float();
 	king_FXid = app->audio->LoadFx("Assets/Audio/Fx/pig_explosion_FX.wav");
 
 	return true;
@@ -31,51 +30,17 @@ bool Rey::Awake() {
 
 bool Rey::Start() {
 
-
-	////run
-	//runRight.LoadAnimations("Bombright", "cerdoBomba");
-	//runRight.speed = 0.16f;
-
-	//runLeft.LoadAnimations("Bombleft", "cerdoBomba");
-	//runLeft.speed = 0.16f;
-	////dead
-	//dead.LoadAnimations("Dead", "cerdoBomba");
-	//dead.speed = 0.25f;
-	//watifok
-	/*watifokIn.LoadAnimations("Watifokin", "cerdoBomba");
-	watifokIn.speed = 0.05f;*/
-
-	/*currentAnimation = &runRight;*/
-
 	//initilize textures
 	texture = app->tex->Load(texturePath);
 	pbody = app->physics->CreateRectangle(position.x + 50, position.y + 60,100,120, bodyType::STATIC);
 	pbody->ctype = ColliderType::REY;
 	pbody->listener = this;
 
-	//initialTransform = pbody->body->GetTransform();
-
-	//LoadAnimations();
-
 	return true;
 }
 
 bool Rey::Update(float dt)
 {
-
-	//health -= 1;
-
-	/*if (isFollowingPlayer)
-	{
-		app->render->DrawTexture(texture, position.x-10, position.y - 10, &watifokIn.GetCurrentFrame());
-	}*/
-	/*else
-	{
-		app->render->DrawTexture(texture, position.x, position.y + 1, &watifokIn.GetCurrentFrame());
-	}*/
-
-	// Resto de tu código de dibujo...
-
 	app->render->DrawTexture(texture, position.x, position.y);
 	return true;
 }
@@ -92,30 +57,6 @@ void Rey::OnCollision(PhysBody* physA, PhysBody* physB) {
 		app->audio->PlayFx(king_FXid, 0);
 		texturePath = "Assets/Textures/Throneyum.png";
 		texture = app->tex->Load(texturePath);
-		break;
-	case ColliderType::ITEM:
-		LOG("Collision ITEM");
-		break;
-	case ColliderType::DAMAGE:
-		LOG("Collision DAMAGE");
-		break;
-	case ColliderType::PLATFORM:
-		LOG("Collision PLATFORM");
-		break;
-	case ColliderType::WALL:
-		LOG("Collision WALL");
-		break;
-	case ColliderType::CEILING:
-		LOG("Collision CEILING");
-		break;
-	case ColliderType::TRAP:
-		LOG("Collision TRAP");
-		break;
-	case ColliderType::NEXTLEVEL:
-		LOG("Collision NEXTLEVEL");
-		break;
-	case ColliderType::UNKNOWN:
-		LOG("Collision UNKNOWN");
 		break;
 	}
 
--- Platformer-GameDev/Game/Source/Rey.h
+++ Platformer-GameDev/Game/Source/Rey.h
@@ -26,39 +26,11 @@ public:
 
 	void OnCollision(PhysBody* physA, PhysBody* physB);
 
-	void OnDeath();
-
 public:
-	//float speed = 1.0f;
 	const char* texturePath;
 	SDL_Texture* texture = NULL;
-	//SDL_Texture* pathTexture = NULL;
 	int king_FXid;
 	PhysBody* pbody;
-	//iPoint cerdoPosition;
-	//iPoint playerTilePos;
-	//int posA;
-	//int posB;
-	//bool direction = true;
-	//bool isFollowingPlayer = false;
-	//int health = 100;
-	//int pigExplosion_FXid;
-	//int pigOink_FXid;
-	//bool isJumping = false;
-	//bool running = false;
-	//bool isDead = false;
-	//bool left_right = true;
-
-	//Animation* currentAnimation = nullptr;
-	/*Animation idleRight;*/
-	//Animation idleLeft;
-	//Animation runRight;
-	//Animation runLeft;
-	//Animation watifokIn;
-	//Animation attack;
-	//Animation dead;
-	//Animation jumpRight;
-	//Animation jumpLeft;
 };
 
 #endif
